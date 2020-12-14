f14eea7720799f1846bef166f0ebd2d45cee63cb
VihanLeRoux <nighthowler1275@gmail.com>
2020-12-14 12:42:31 +0200

    first commit

---
+++ urls.py
@@ -0,0 +1,33 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib.auth import views as django_views
+
+urlpatterns = [
+    path('', include('core.urls')),
+    path('blog/', include('blog.urls')),
+    path('accounts/', include('user_profile.urls')),
+    path('chess/', include('chess.urls')),
+    path('cypher/', include('cypher.urls')),
+    path('search/', include('query.urls')),
+    path('login/', django_views.LoginView.as_view(), name='login'),
+    path('logout/', django_views.LogoutView.as_view(), name='logout'),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
